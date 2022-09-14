0c6709f755080c512db3b587216a47f935d63b29
level237 <Bramslevel129@gmail.com>
2022-09-14 09:59:28 -0400

    Type aliases

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,46 @@
+/**
+ * *****Objects
+
 let employee:{
     readonly id:number,
     name?:string,
     retire:(date:Date)=>void
-}={id:1,name:'level',retire:(date:Date)=>{
+}={
+    id:1,
+    name:'level',
+    retire:(date:Date)=>{
     console.log(date)
 }};
 employee.name="martin";
+ */
+
+
+/****
+ * 
+ * Section 2: Advanced Learn to typescript
+ * Type aliases
+ * Union and Intersections
+ * Type narrowing
+ * nullable type
+ * the never type
+ * 
+ */
+
+/*********
+ * Type aliases
+ * DRY: Don't repeat Yourself
+ 
+
+type Employee={
+    readonly id:number,
+    name:string,
+    retire:(date:Date) =>void
+}
+
+ let employee:Employee={
+    id:1,
+    name:'level',
+    retire:(date:Date)=>{
+    console.log(date)
+}};
+*/
